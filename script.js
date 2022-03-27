f657e553113d851e82c316ff3a3917ffa58039d9
Alex Tikhonov <tau9501@gmail.com>
2022-03-27 21:26:00 +0300

    мой первый коммит

---
+++ script.js
@@ -0,0 +1,8 @@
+const title = 1;
+const screens = 2;
+const screenPrice = 3;
+const rollback = 4;
+const fullPrice = 5;
+const adaptiv = 6;
+alert("Иисусе, кому может нравится GIT?");
+console.log("Hello, GIT!");

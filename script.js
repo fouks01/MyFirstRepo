974f9fca138eb96d9219f32da69280b90a826a0e
Alex Tikhonov <tau9501@gmail.com>
2022-04-01 21:41:56 +0300

    Fixed lesson03

--- script.js
+++ script.js
@@ -1,51 +1,16 @@
 'use strict';
 
-const title = prompt('Как называется ваш проект?');
-// const titleResult = "Mytitle";
-console.log(title);
-// const title = "Mytitle";
-// console.log(typeof title);
-
-
-const screens = +prompt("Какие типы экранов нужно разработать?");
-const screensResult = "Простые, Сложные, Интерактивные";
-console.log(screensResult);
-// const screens = "Простые, Сложные, Интерактивные";
-// console.log(screens.length);
-// console.log(screens.toLocaleLowerCase().split(", "));
-
-const screenPrice = +prompt('Сколько будет стоить данная работа?');
-// const screenPriceResult = 12000;
-console.log("Стоимость верстки экранов " + screenPrice + " юайней");
-
-const service1 = prompt('Какой дополнительный тип услуги нужен?');
-// const service1Result = "Отдых";
-console.log(service1);
-
-const servicePrice1 = +prompt('Сколько это будет стоить?');
-// const servicePrice1Result = 15000;
-console.log(servicePrice1);
-
-const service2 = prompt('Какой дополнительный тип услуги нужен?');
-// const service2Result = "Покупка путевок";
-console.log(service2);
-
-const servicePrice2 = +prompt('Сколько это будет стоить?');
-// const servicePrice2Result = 23000;
-console.log(servicePrice2);
-
+const title = prompt('Как называется ваш проект?', "Калькулятор верстки");
+const screens = +prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = +prompt('Сколько будет стоить данная работа?', '12000');
+const service1 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка сайта');
+const servicePrice1 = +prompt('Сколько это будет стоить?', '10000');
+const service2 = prompt('Какой дополнительный тип услуги нужен?', "Слайдеры");
+const servicePrice2 = +prompt('Сколько это будет стоить?', '10000');
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 const rollback = 12;
-console.log(typeof fullPrice);
-console.log('Стоимость разработки сайта ' + fullPrice + ' юаней');
-console.log("Процент отката посреднику за работу = " + fullPrice * (rollback / 100));
-console.log(fullPrice);
-
 let adaptiv = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptiv);
-
 let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
-console.log(Math.ceil(servicePercentPrice));
 
 if (fullPrice > 30000) {
   console.log("Даем скидку в 10% " + Math.ceil(servicePercentPrice * 0.1));
@@ -56,3 +21,17 @@ if (fullPrice > 30000) {
 } else if (fullPrice < 0) {
   console.log("Что то пошло не так ");
 }
+
+console.log(title);
+console.log(screens);
+console.log("Стоимость верстки экранов " + screenPrice + " юайней");
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(typeof fullPrice);
+console.log('Стоимость разработки сайта ' + fullPrice + ' юаней');
+console.log("Процент отката посреднику за работу = " + fullPrice * (rollback / 100));
+console.log(fullPrice);
+console.log(adaptiv);
+console.log(Math.ceil(servicePercentPrice));

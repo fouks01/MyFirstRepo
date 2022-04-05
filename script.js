d4157850b47b17e346792f064442d56184f7a53d
Alex Tikhonov <tau9501@gmail.com>
2022-04-05 15:35:07 +0300

    Added lesson08

--- script.js
+++ script.js
@@ -2,57 +2,91 @@
 
 const appData = {
   title: '',
-  screens: '',
+  screens: [],
   screenPrice: 0,
   fullPrice: 0,
   allServicePrices: 0,
   rollback: 12,
   adaptiv: true,
   servicePercentPrice: 0,
-  service1: '',
-  service2: '',
+  services: {},
+
   asking: function () {
-    appData.title = prompt('Как называется ваш проект?', "Калькулятор верстки");
-    appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+    // appData.title = prompt('Как называется ваш проект?', "Калькулятор верстки");
 
     do {
-      appData.screenPrice = prompt('Сколько будет стоить данная работа?');
+      appData.title = prompt('Как называется ваш проект?', "Калькулятор верстки");
     }
-    while (!appData.isNumber(appData.screenPrice));
+    while (appData.isNumber(appData.title));
 
-    appData.adaptiv = confirm("Нужен ли адаптив на сайте?");
-  },
-  getAllServicePrices: function () {
-    let sum = 0;
-    let newre;
+    // appData.screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+
+    // do {
+    //   appData.screenPrice = prompt('Сколько будет стоить данная работа?');
+    // }
+    // while (!appData.isNumber(appData.screenPrice));
 
     for (let i = 0; i < 2; i++) {
+      let name;
+      let price = 0;
 
-      if (i === 0) {
-        appData.service1 = prompt('Какой дополнительный тип услуги нужен?', 'Верстка сайта');
-      } else if (i === 1) {
-        appData.service2 = prompt('Какой дополнительный тип услуги нужен?', 'Слайдер');
+      do {
+        name = prompt("Какие типы экранов нужно разработать?");
       }
+      while (appData.isNumber(name));
+
+
+      do {
+        price = prompt('Сколько будет стоить данная работа?');
+      }
+      while (!appData.isNumber(price));
+
+      appData.screens.push({
+        id: i,
+        name: name,
+        price: price
+      });
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let name;
+      let newre = 0;
+
+      do {
+        name = prompt('Какой дополнительный тип услуги нужен?', 'Верстка сайта');
+      }
+      while (appData.isNumber(name));
 
       do {
         newre = prompt('Сколько это будет стоить?', '10000');
       }
       while (!appData.isNumber(newre));
-      sum += +newre;
+
+      appData.services[name] = +newre;
     }
-    return sum;
+    appData.adaptiv = confirm("Нужен ли адаптив на сайте?");
   },
+  addPrices: function () {
+    for (let screen of appData.screens) {
+      appData.screenPrice += +screen.price;
+    }
+
+    for (let key in appData.services) {
+      appData.allServicePrices += appData.services[key];
+    }
+  },
+
   getFullPrice: function (a, b) {
-    return a + b;
+    appData.fullPrice = a + b;
   },
   getTitle: function (title) {
     appData.title = appData.title.trim();
     appData.title = appData.title.toLowerCase();
     appData.title = appData.title[0].toUpperCase() + title.substring(1);
-    return appData.title;
+    appData.title = appData.title;
   },
   getServicePercentPrices: function (a, b) {
-    return a - a * (b / 100);
+    appData.servicePercentPrice = a - a * (b / 100);
   },
   getRollbackMessage: function (price) {
     if (price >= 30000) {
@@ -70,16 +104,17 @@ const appData = {
   },
   start: function () {
     appData.asking();
-    appData.allServicePrices = appData.getAllServicePrices();
-    appData.fullPrice = appData.getFullPrice(+appData.screenPrice, appData.allServicePrices);
-    appData.title = appData.getTitle(appData.title);
-    appData.servicePercentPrice = appData.getServicePercentPrices(appData.fullPrice, appData.rollback);
+    appData.addPrices();
+    appData.getFullPrice(+appData.screenPrice, appData.allServicePrices);
+    appData.getTitle(appData.title);
+    appData.getServicePercentPrices(appData.fullPrice, appData.rollback);
     appData.logger();
   },
   logger: function () {
     for (let key in appData) {
       console.log(appData[key]);
     }
+    console.log(appData.screens);
   }
 };
 

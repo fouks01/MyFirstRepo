23f1b0b70b0ec2e515a3f46182ec269e1e70ce92
Alex Tikhonov <tau9501@gmail.com>
2022-04-06 23:06:59 +0300

    Added lesson09

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const titleh1 = document.getElementsByTagName('h1');
+console.log(titleh1[0].innerText);
+
+const cancel = document.getElementsByClassName('handler_btn');
+console.log(cancel.start, cancel.reset);
+
+const plus = document.querySelector('.screen-btn');
+console.log(plus);
+
+const items = document.querySelectorAll('.other-items.percent');
+const another = document.querySelectorAll('.other-items.number');
+console.log(items, another);
+
+const input = document.querySelector('.rollback input[type=range]');
+console.log(input);
+
+const ran = document.querySelector('.rollback span.range-value');
+console.log(ran);
+
+const total = document.getElementsByClassName('total-input');
+const total0 = total[0];
+const total1 = total[1];
+const total2 = total[2];
+const total3 = total[3];
+const total4 = total[4];
+console.log(total0, total1, total2, total3, total4);
+
+let screen = document.querySelectorAll('.screen');
+console.log(screen);
+
 const appData = {
   title: '',
   screens: [],
